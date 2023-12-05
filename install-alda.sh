6a320f151f558ac917447056858d9c01f578aa0f
Preston Johnson <131288509+preston-johnson@users.noreply.github.com>
2023-12-05 01:55:28 +0000

    Check source exists before downloading

--- install-alda.sh
+++ install-alda.sh
@@ -1,7 +1,13 @@
-wget https://alda-releases.nyc3.digitaloceanspaces.com/2.2.7/client/linux-amd64/alda
-wget https://alda-releases.nyc3.digitaloceanspaces.com/2.2.7/player/non-windows/alda-player
+if ! test -f /workspaces/alda/alda; then
+  wget https://alda-releases.nyc3.digitaloceanspaces.com/2.2.7/client/linux-amd64/alda
+fi
 
 chmod +x alda
+
+if ! test -f /workspaces/alda/alda-player; then
+    wget https://alda-releases.nyc3.digitaloceanspaces.com/2.2.7/player/non-windows/alda-player
+fi
+
 chmod +x alda-player
 
 export PATH="/workspaces/alda:$PATH"
